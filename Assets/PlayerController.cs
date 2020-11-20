eaeddc84180d332ebd8caf28a90a6f92418cfac7
jlaassri <jordanlaassri@hotmail.com>
2020-11-20 03:56:40 +0000

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int CurrHealth;
+
+    public int Dmg = 10;
+
+    public int MovementSpeed = 5;
+
+
+    void Start()
+    {
+        CurrHealth = MaxHealth;
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CompareTag("enProj"))
+        {
+            CurrHealth -= enDmg;
+        }  
+        
+
+    }
+}
