2d8e6781cd3a320b5c236b615c8628ea68b508c7
jlaassri <jordanlaassri@hotmail.com>
2020-11-21 03:32:39 +0000

    fixxed health bug

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -23,10 +23,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (CompareTag("enProj"))
-        {
-            CurrHealth -= enDmg;
-        }  
+        
         
 
     }
