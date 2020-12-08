5c74b97cec078a1d74c1c7fb31bc6c149311b367
jlaassri <jordanlaassri@hotmail.com>
2020-12-08 02:28:40 +0000

    small additions to player and enemy starts

---
+++ Assets/PlayerShooter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    public float FireDelay = 0.025f;
+    float cooldowntimer = 0;
+
+    void Update()
+    {
+        cooldowntimer -= Time.deltaTime;
+
+        if (Input.GetButton("Fire") && cooldowntimer <= 0)
+        {
+            Debug.Log("fire");
+            cooldowntimer = FireDelay;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Dmg.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dmg : MonoBehaviour
+{
+    public int MaxHealth = 1;
+    public int CurrHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrHealth = MaxHealth;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
