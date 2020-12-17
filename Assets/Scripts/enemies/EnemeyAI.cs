e2001e901683b654a0aafd4eeb273278d95d78d5
jlaassri <jordanlaassri@hotmail.com>
2020-12-17 03:00:35 +0000

    Update EnemeyAI.cs

--- Assets/Scripts/enemies/EnemeyAI.cs
+++ Assets/Scripts/enemies/EnemeyAI.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class EnemeyAI : MonoBehaviour
 {
-   
-    
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+
+    }
+
 }
