60d0173cab09ed5c34c9a30c71eb4e8746b7ee36
jlaassri <jordanlaassri@hotmail.com>
2021-05-26 04:11:40 +0100

    move code around and commented

--- Assets/Scripts/enemies/PlayerLook.cs
+++ Assets/Scripts/enemies/PlayerLook.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    private GameObject player = null;
+    private GameObject player = null; //gets player game object
 
     private void Start()
     {
-        if (player == null)
-            player = GameObject.Find("Player");
+        if (player == null)// checks if gameobject is empty
+            player = GameObject.Find("Player"); //sets the player gameobject to the player 
     }
     public void Update()
     {
-        //Vector2 dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-        Vector2 dir = (Camera.main.ScreenToWorldPoint(player.transform.position) - transform.position).normalized;
-        transform.right = dir;
+        Vector2 dir = (Camera.main.ScreenToWorldPoint(player.transform.position) - transform.position).normalized; //sets dir vector2 to the players position 
+        transform.right = dir;// transfroms object to target rotation
     }
 }
---
+++ Assets/Scripts/enemies/emSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emSpawner : MonoBehaviour
+{
+
+    private float timebtwspawn; // time between spawns
+    public float strartimer; // set timer 
+    public static float limmiter = 0; //spawn limmiter
+
+    public LootTable thisloot; //lootable
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (limmiter > 25) //limmiter check 
+        {
+            limmiter = 25; //if limmiter is > 25, set to 25
+        }
+        if (timebtwspawn <= 0 & limmiter < 25)// checks if time between shoots & limmiter is within range 
+        {
+            Debug.Log("emSpawned"); //debug for checking
+            MakeItems(); //runs enemy spawner loot tables
+            timebtwspawn = strartimer; //reset timer
+        }
+        else
+        {
+            timebtwspawn -= Time.deltaTime; //time passing call
+        }
+    }
+
+    private void MakeItems()// spawner function
+    {
+        if (thisloot != null) //checks if loot  table is empty
+        {
+            GameObject current = thisloot.LootPowerup(); //gets result from the loot table
+            if (current != null) //checks if loot table has any retruns
+            {
+                Instantiate(current.gameObject, transform.position, Quaternion.identity); //spawns returned enemies
+                limmiter++; //increases limmiter value by 1
+            }
+        }
+    }
+
+}
